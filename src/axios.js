16e7b27ea98c9f1e719c74dad9b708e507a02b49
yadavanurag <anuragyadav13481@gmail.com>
2020-03-29 18:01:39 +0530

    restructure the project to have different pages

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.comm'
+  baseURL: 'https://jsonplaceholder.typicode.com'
 });
 instance.defaults.headers.common['Authorization'] = 'AUTH TOKEN FROM INSTANCE';
 
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,56 +1,24 @@
 import React, { Component } from 'react';
-import axios from '../../axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import Posts from './Posts/Posts';
 
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    error: false
-  }
-
-  componentDidMount(){
-    axios.get('/posts')
-      .then(response => {
-        const posts = response.data.slice(0, 4);
-        const updatedPosts = posts.map(post => ({...post, author: 'Anu'}));
-
-        this.setState({posts: updatedPosts});
-      })
-      .catch(error => this.setState({error: true}));
-  }
-  postSelectedHandler = (id) => this.setState({selectedPostId: id});
 
   render () {
-    let posts = <p style={{textAlign: 'center'}}>Something went wrong...!</p>;
-    if(!this.state.error){
-      posts = this.state.posts
-        .map(post => (
-          <Post
-            key={post.id}
-            title={post.title}
-            author={post.author}
-            clicked={() => this.postSelectedHandler(post.id)}/>
-        )
-      );
-    }
 
     return (
       <div>
-        <section className="Posts">
-          {posts}
-        </section>
-        <section>
-          <FullPost id={this.state.selectedPostId} />
-        </section>
-        <section>
-          <NewPost />
-        </section>
+        <header className='Blogs'>
+          <nav>
+            <ul>
+              <li><a href='/' >Home</a></li>
+              <li><a href='/new-post' >New Post</a></li>
+            </ul>
+          </nav>
+        </header>
+        <Posts />
       </div>
     );
   }
