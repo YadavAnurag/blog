5152e5ec581851d421d6da4a18392375d5ee0905
yadavanurag <anuragyadav13481@gmail.com>
2020-03-30 18:59:59 +0530

    added HOC asyncComponent for lazy loading to optimize performance on big application

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
 import { Route, NavLink, withRouter, Switch, Redirect } from 'react-router-dom';
+import asyncComponent from '../../hoc/asyncComponent';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
 import './Blog.css';
 
+const AsyncNewPost = asyncComponent(() => {
+  return import('./NewPost/NewPost');
+});
 
 
 class Blog extends Component {
@@ -35,7 +38,7 @@ class Blog extends Component {
         {/* <Route path='/' exact render={() => <h1>Home</h1>} />
         <Route path='/new-post' exact render={() => <h1>New Post</h1>} /> */}
         <Switch>
-          <Route path='/new-post' component={NewPost} />
+          <Route path='/new-post' component={AsyncNewPost} />
           <Route path='/posts' component={Posts} />
           <Redirect from='/' to='/posts' />
         </Switch>
